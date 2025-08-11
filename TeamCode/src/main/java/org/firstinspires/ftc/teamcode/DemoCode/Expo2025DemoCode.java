2428192118804df3c27a0787917f8f3e02efc441
Pr0totyp31 <yesquer@gregoryschool.org>
2025-08-10 17:15:50 -0700

    Haha, whoops, I forgot to stage the changes
    Nvm, just ignore this message...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoCode/Expo2025DemoCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoCode/Expo2025DemoCode.java
@@ -1,4 +1,143 @@
 package org.firstinspires.ftc.teamcode.DemoCode;
 
-public class Expo2025DemoCode {
+//import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+//For FLL scrimmage
+//@Disabled
+@TeleOp(name = "ExpoDrive", group = "ExpoDrive")
+
+public class Expo2025DemoCode extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotor linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
+        linearSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+        linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // Makes the motors output their rotation
+        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Motor Setup
+        DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        DcMotor leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
+        DcMotor rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        DcMotor rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        // Sets the motor direction
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Makes the motors stop moving when they receive an input of 0
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Set up FtcDashboard telemetry
+        //FtcDashboard dashboard = FtcDashboard.getInstance();
+        //Telemetry dashboardTelemetry = dashboard.getTelemetry();
+
+        double changeInSpeed = 0.35;
+
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT, // Change to left if doesn't work
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+
+        while (!isStarted()) {
+
+        }
+
+        while (opModeIsActive()) {
+            // Define joystick controls
+            double moveSlide = -gamepad1.right_stick_y;
+
+            // Drive
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rot = gamepad1.right_stick_x;
+
+            boolean slowDown = gamepad1.right_bumper || gamepad1.right_trigger > 0.1;
+
+            if (gamepad1.dpad_up) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bots rotation
+            double strafe = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double drive = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+            strafe = -strafe; // here's the strafe inversion
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(rot), 1);
+            double frontLeftPower = (drive + strafe + rot) / denominator;
+            double backLeftPower = (drive - strafe + rot) / denominator;
+            double frontRightPower = (drive - strafe - rot) / denominator;
+            double backRightPower = (drive + strafe - rot) / denominator;
+            //telemetry.addData("fl, bl,fr,br: ", "%.2f %.2f %.2f %.2f",frontLeftPower,backLeftPower,frontRightPower,backRightPower);
+
+            if (slowDown) {
+                frontLeftPower *= changeInSpeed;
+                frontRightPower *= changeInSpeed;
+                backLeftPower *= changeInSpeed;
+                backRightPower *= changeInSpeed;
+            }
+
+            double roundDown = 0.05;
+            if (Math.abs(frontLeftPower) <= roundDown) {
+                frontLeftPower = 0;
+            }
+            if (Math.abs(frontRightPower) <= roundDown) {
+                frontRightPower = 0;
+            }
+            if (Math.abs(backLeftPower) <= roundDown) {
+                backLeftPower = 0;
+            }
+            if (Math.abs(backRightPower) <= roundDown) {
+                backRightPower = 0;
+            }
+
+            leftFrontDrive.setPower(frontLeftPower);
+            leftBackDrive.setPower(backLeftPower);
+            rightFrontDrive.setPower(frontRightPower);
+            rightBackDrive.setPower(backRightPower);
+
+            //////////////// OTHER COMPONENTS //////////////////
+            if (linearSlide.getCurrentPosition() >= 2210 && moveSlide > 0) {
+                moveSlide = 0;
+            } else if (linearSlide.getCurrentPosition() <= 2 && moveSlide < 0) {
+                moveSlide = 0;
+            } else if (moveSlide == 0 && linearSlide.getCurrentPosition() >= 0) {
+                moveSlide = 0.1;
+            }
+            double linearSlowDown = 0.75;
+            linearSlide.setPower(moveSlide * linearSlowDown);
+
+            telemetry.addData("Linear Slide", linearSlide.getCurrentPosition());
+
+            YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+            telemetry.addData("Pitch (X)", "%.2f", orientation.getPitch(AngleUnit.DEGREES));
+            telemetry.addData("Roll (Y)", "%.2f", orientation.getRoll(AngleUnit.DEGREES));
+            telemetry.addData("Yaw (Z)", "%.2f", orientation.getYaw(AngleUnit.DEGREES));
+            telemetry.update();
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/ServoTelemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/ServoTelemetry.java
@@ -1,4 +1,69 @@
 package org.firstinspires.ftc.teamcode.Tools;
 
-public class ServoTelemetry {
+/*
+ADD 'acmerobotics' INSIDE 'build.dependencies.gradle',
+CHECK THE 'IntoTheDeep2' REPO FOR A BETTER IDEA OF WHAT TO DO,
+ASK MR. CONNER FOR ADDITIONAL HELP
+(SINCE LAST YEAR HE SAID HE HAD TO CHANGE SOME WEIRD NUMBERS ALSO TO GET IT TO WORK)
+*/
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+//import com.acmerobotics.dashboard.FtcDashboard;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name = "ServoTelemetry", group = "Tools")
+
+public class ServoTelemetry extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Servo servo = hardwareMap.get(Servo.class, "arm_servo");
+
+        while (!isStarted()) {
+            
+        }
+
+        // Keeps track of servo position...
+        double servoPosition = servo.getPosition();
+
+        // These are used to check if a button is being held down.
+        boolean smallButtonEcho = false;
+        boolean mediumButtonEcho = false;
+        boolean largeButtonEcho = false;
+
+        while (opModeIsActive()) {
+
+            // Change these values to change the increment amount.
+            double smallIncrementValue = 0.05;
+            double mediumIncrementValue = smallIncrementValue * 5.0; // 0.25
+            double largeIncrementValue = mediumIncrementValue * 5.0; // 1.25
+
+            // Update inputs.
+            boolean smallButtonPress = gamepad1.x;
+            boolean mediumButtonPress = gamepad1.b;
+            boolean largeButtonPress = gamepad1.a;
+
+            // Update servo position variable.
+            if (smallButtonPress && !smallButtonEcho) {
+                servoPosition += smallIncrementValue;
+            }
+            if (mediumButtonPress && !mediumButtonEcho) {
+                servoPosition += mediumIncrementValue;
+            }
+            if (largeButtonPress && !largeButtonEcho) {
+                servoPosition += largeIncrementValue;
+            }
+
+            // Update echo.
+            smallButtonEcho = smallButtonPress;
+            mediumButtonEcho = mediumButtonPress;
+            largeButtonEcho = largeButtonPress;
+
+            servo.setPosition(servoPosition); // Sets the servo position...
+            telemetry.addData("Servo Position: %.2f", servoPosition); // I think this'll print correctly...
+            telemetry.update();
+        }
+    }
 }
