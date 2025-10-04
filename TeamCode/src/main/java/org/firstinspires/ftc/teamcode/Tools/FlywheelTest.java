7241767bbd37bc278a38a56eb650dd9a8304c7fa
Pr0totyp31 <yesquer@gregoryschool.org>
2025-10-03 18:11:28 -0700

    Added sweep_servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/FlywheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/FlywheelTest.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Tools;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 @TeleOp(name = "FlywheelTest", group = "Tools")
 
 public class FlywheelTest extends LinearOpMode {
@@ -20,11 +22,22 @@ public class FlywheelTest extends LinearOpMode {
         leftFlywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFlywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        Servo sweepServo = hardwareMap.get(Servo.class, "sweep_servo");
+
         while (!isStarted()) {
 
         }
 
         while (opModeIsActive()) {
+            boolean sweep = gamepad1.a;
+            boolean reset = gamepad1.b;
+
+            if (sweep) {
+                sweepServo.setPosition(1);
+            } else if (reset) {
+                sweepServo.setPosition(0);
+            }
+
             leftFlywheel.setPower(1.0);
             rightFlywheel.setPower(1.0);
         }
